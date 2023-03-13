fcaf09216f11a329151ed6f31a0168367c5e7dc1
kanglf20 <kanglf20@mails.tsinghua.edu.cn>
2023-03-13 21:54:07 +0800

    start menu

---
+++ scripts/start.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class start : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
