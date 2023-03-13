47f4660f71df285847103f851b10996ab93da73c
kanglf20 <kanglf20@mails.tsinghua.edu.cn>
2023-03-14 01:14:15 +0800

    add submenu:startmenu/settings && add volume control and looping BGM

--- scripts/main.cs
+++ scripts/main.cs
@@ -6,10 +6,22 @@ public partial class main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetNode<AudioStreamPlayer2D>("BGM").Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+	}
+
+	public void OnVolumeSettingChanged(float value)
+	{
+		GetNode<AudioStreamPlayer2D>("BGM").VolumeDb = value == 0.001f ? -100.0f : Mathf.Log(value) * 5.0f;
+	}
+
+	public void OnBGMFinished()
+	{
+		GetNode<AudioStreamPlayer2D>("BGM").Play();
 	}
 }
--- scripts/start.cs
+++ scripts/start.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class start : Node
 {
+	[Signal]
+	public delegate void VolumeChangedEventHandler(float value);
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetNode<CanvasLayer>("CanvasLayer/StartMenu").Show();
+		GetNode<CanvasLayer>("CanvasLayer/SettingsMenu").Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,8 +17,23 @@ public partial class start : Node
 	{
 	}
 
-	public void OnQuitButtonPressed()
+	public void OnQuitButtonPressed() => GetTree().Quit();
+
+	public void OnSettingsButtonPressed()
 	{
-		GetTree().Quit();
+		GetNode<CanvasLayer>("CanvasLayer/StartMenu").Hide();
+		GetNode<CanvasLayer>("CanvasLayer/SettingsMenu").Show();
 	}
+
+	public void OnSettingsBackButtonPressed()
+	{
+		GetNode<CanvasLayer>("CanvasLayer/StartMenu").Show();
+		GetNode<CanvasLayer>("CanvasLayer/SettingsMenu").Hide();
+	}
+
+	public void OnVolumeSettingChanged(float value)
+	{
+		EmitSignal(SignalName.VolumeChanged, value);
+	}
+	public void Hide() => GetNode<CanvasLayer>("CanvasLayer").Hide();
 }
