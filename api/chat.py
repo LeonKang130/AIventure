5eca7fffdb237500870391d087b17c804fcc013a
遆周杰 <2020050008@secoder.net>
2023-03-19 18:26:39 +0800

    更新chat方式

--- api/chat.py
+++ api/chat.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 prompt = """
 你好，请你介绍一下chatGPT
--- api/image.py
+++ api/image.py
@@ -1,11 +1,24 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
+#translation
+chinese_text='很多蝴蝶在粉色的花丛中飞舞'#接受用户输入的中文字符串
+translation = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": f'Translate the following chinese text to Engilish: "{chinese_text}"'}
+  ]
+)
+
+print(translation.choices[0].message.content)
+English_text = translation.choices[0].message.content
+
+#image
 response = openai.Image.create(
-  prompt="钢铁做的刀，木制的刀把", #A text description of the desired image(s). The maximum length is 1000 characters.
-  n=3, #The number of images to generate. Must be between 1 and 10.
-  size="1024x1024" # Must be one of 256x256, 512x512, or 1024x1024
+  prompt=English_text, #A text description of the desired image(s). The maximum length is 1000 characters.
+  n=1, #The number of images to generate. Must be between 1 and 10.
+  size="256x256" # Must be one of 256x256, 512x512, or 1024x1024
 )
 for i in response['data']:
-  print(i)
+  print(i)#图片url
