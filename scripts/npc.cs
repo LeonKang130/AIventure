ba6ffc87bfbc3eee05d19c85ce6ec11d676c2ef2
项楚钧 <2020011021@secoder.net>
2023-03-23 23:15:18 +0800

    Implement the basic function of dialog system

--- scripts/npc.cs
+++ scripts/npc.cs
@@ -26,7 +26,20 @@ public partial class npc : Area2D
 		Triggered = true;
 		_emotion.Show();
 		_emotion.Frame = 0;
-	}
+
+        // Wrong
+        // if (Input.IsActionPressed("start_continue_dialog"))
+        // {
+        //     GetParent<CanvasLayer>()
+        //     .GetParent<wander>()
+        //     .GetNode<CanvasLayer>("Dialog").Show();
+
+        //     GetParent<CanvasLayer>()
+        //     .GetParent<wander>()
+        //     .GetNode<Label>("Dialog/HSplitContainer/TextMargin/Label").Text
+        //     = "Harold: What the hell??? Where am I??? Who are you???";
+        // }
+    }
 
 	public void OnPlayerExitArea(Node2D _)
 	{
--- scripts/wander.cs
+++ scripts/wander.cs
@@ -4,25 +4,69 @@ using Godot.Collections;
 
 public partial class wander : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    string npc = "Dialog/HSplitContainer/TextMargin/Label";
+    string player = "Dialog/HSplitContainer/TextMargin/TextEdit";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 	}
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (GetNode<CanvasLayer>("Dialog").Visible && Input.IsActionPressed("skip_dialog"))
-		{
-			GetNode<CanvasLayer>("Dialog").Hide();
-		}
+        // Skip dialog while player is listening
+        if (GetNode<Label>(npc).Visible && Input.IsActionPressed("skip_dialog"))
+        {
+            GetNode<CanvasLayer>("Dialog").Hide();
+            GetNode<Label>(npc).Text = "???: Back again?";  // To Be Implemented
+        }
 
-		var material = GetNode<ColorRect>("CanvasLayer/Vignette").Material as ShaderMaterial;
+        // Enter the process of dialog, and start to talk
+        // NPC: Listening
+        // Player: Talking
+        if (GetNode<Label>(npc).Visible && Input.IsActionPressed("start_continue_dialog"))
+        {
+            GetNode<Label>(npc).Hide();
+            GetNode<TextEdit>(player).Show();
+        }
+
+        // Player finishes talking
+        // NPC: Responding
+        // Player: Listening
+        if (GetNode<TextEdit>(player).Visible && Input.IsActionPressed("end_text_edit"))
+        {
+            string player_words = GetNode<TextEdit>(player).Text;
+            GetNode<TextEdit>(player).Text = "";
+            GetNode<TextEdit>(player).Hide();
+            GetNode<Label>(npc).Text = "???: It sounds like nonsense...";  // To Be Implemented
+            GetNode<Label>(npc).Show();
+        }
+
+        // Forced escape dialog  BUT CANNOT FUCKING TYPE Q
+        // if (GetNode<TextEdit>(player).Visible && Input.IsActionPressed("skip_dialog"))
+        // {
+        //     GetNode<TextEdit>(player).Text = "";
+        //     GetNode<TextEdit>(player).Hide();
+        //     GetNode<Label>(npc).Text = "???: Back again?";  // To Be Implemented
+        //     GetNode<Label>(npc).Show();
+        // 	GetNode<CanvasLayer>("Dialog").Hide();
+        // }
+
+        // Escape Game: Back to the "Main" Scene
+        if (Input.IsActionPressed("escape"))
+        {
+            GetParent<main>().GetNode<wander>("Wander").Hide();
+            GetParent<main>().GetNode<start>("Start").Show();
+        }
+
+        // Shader
+        var material = GetNode<ColorRect>("CanvasLayer/Vignette").Material as ShaderMaterial;
 		if (material != null)
 		{
 			material.SetShaderParameter("player_pos", GetNode<player>("CanvasLayer/Player").Position / 1024);
 		}
-	}
+    }
 
 	public void Hide()
 	{
@@ -34,5 +78,6 @@ public partial class wander : Node
 	public void OnEnterPortal(Node2D _)
 	{
 		GetNode<CanvasLayer>("Dialog").Show();
-	}
+        GetNode<Label>(npc).Text = "???: Where do you think you are going?";
+    }
 }
