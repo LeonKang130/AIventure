7a467f5a16bf6b05b2023ddd123e82420bea30d9
遆周杰 <2020050008@secoder.net>
2023-04-19 17:54:13 +0800

    完成后端chat功能

--- express/chat.py
+++ express/chat.py
@@ -1,20 +1,32 @@
 import openai
-import time
-start_time = time.time()
+# import time
+import sys
+import json
+# input = sys.argv[1].replace('\\n','')
+input = sys.argv[1]
+messages = json.loads(json.loads(input))#获取到前端的messages
+# print('*' * 50)
+# print(input)
+# print('*' * 50)
+# print(messages)
+# print(type(input))
+# print(type(messages))
+# messages =[
+#         {"role": "system", "content": "你扮演一个大魔王"},
+#         {"role": "user", "content": "告诉我宝藏在哪"},
+#         {"role": "assistant", "content": "你这个愚蠢的凡人，竟然敢向我询问宝藏的下落？要得到宝藏，你需要先证明自己的实力，否则你只会死无葬身之地。"},
+#         {"role": "user", "content": "你是谁？"},
+#         {"role": "assistant", "content": "我是被称为无尽夜幕的大魔王，我统领着一支强大的魔族军队，掌握着无比的黑暗魔法。我是恐惧与阴影的化身，不是你这个凡人能想象得到的存在。"},
+#         {"role": "user", "content": "大魔王有什么了不起的，我要与你决一死战！"},
+#         # {"role": "assistant", "content": "哈哈哈哈，你这个自不量力的凡人居然敢与我对抗，真是可笑。不过既然你如此热血，那我就奉陪到底吧。来吧，让我看看你有多少实力！"},
+#     ]
+# start_time = time.time()
 #目前只能回复一句话，但效果最好，后期需要想办法将ai回复和用户敲击键盘作为输入append到message中再与api交互
 openai.api_key = "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"
 
 response=openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "你扮演一个大魔王"},
-        {"role": "user", "content": "告诉我宝藏在哪"},
-        {"role": "assistant", "content": "你这个愚蠢的凡人，竟然敢向我询问宝藏的下落？要得到宝藏，你需要先证明自己的实力，否则你只会死无葬身之地。"},
-        {"role": "user", "content": "你是谁？"},
-        {"role": "assistant", "content": "我是被称为无尽夜幕的大魔王，我统领着一支强大的魔族军队，掌握着无比的黑暗魔法。我是恐惧与阴影的化身，不是你这个凡人能想象得到的存在。"},
-        {"role": "user", "content": "大魔王有什么了不起的，我要与你决一死战！"},
-        # {"role": "assistant", "content": "哈哈哈哈，你这个自不量力的凡人居然敢与我对抗，真是可笑。不过既然你如此热血，那我就奉陪到底吧。来吧，让我看看你有多少实力！"},
-    ]
+    model = "gpt-3.5-turbo",
+    messages = messages
 )
 # response=openai.ChatCompletion.create(
 #     model="gpt-3.5-turbo",
@@ -29,7 +41,7 @@ response=openai.ChatCompletion.create(
 #     ]
 # )
 print(response.choices[0].message.content)
-end_time=time.time()
-time = end_time-start_time
-print("代码执行时间",time)
-# https://platform.openai.com/account/api-keys
+# end_time=time.time()
+# time = end_time-start_time
+# print("代码执行时间",time)
+
